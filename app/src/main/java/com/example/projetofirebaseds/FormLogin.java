72bd2603379ddbb2d8c1d5c97d10f3a8762ed841
ALEX SAMI ROCHA RIBEIRO <232.933622@senaicimatec.edu.br>
2024-05-23 16:45:48 -0300

    Initial commit

---
+++ app/src/main/java/com/example/projetofirebaseds/FormLogin.java
@@ -0,0 +1,16 @@
+package com.example.projetofirebaseds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.projetofirebase.R;
+
+public class FormLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_login);
+    }
+}
